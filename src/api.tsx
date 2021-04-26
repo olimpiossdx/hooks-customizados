497f40e248ca2a5db508abb76e9cab6acc9a7b2d
Olimpio <olimpiodx@gmail.com>
2021-04-26 12:36:55 -0300

    Update api.tsx
    Atulizando apiService de teste

--- src/api.tsx
+++ src/api.tsx
@@ -1,15 +1,38 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 import { INotification, IResponseError } from "./App";
 
+interface IApiServiceConfig extends AxiosRequestConfig {
+  retry?: number;
+  retryDelay?: number;
+};
+
+let cancellationToken = axios.CancelToken.source();
+
+export const cancellationRequest = (cancellationReason: string) => {
+  cancellationToken.cancel(cancellationReason);
+};
+
+export const newCancellationToken = () => {
+  cancellationToken = axios.CancelToken.source();
+};
+
+const api: AxiosInstance = axios.create({ baseURL: 'http://localhost:3333', cancelToken: cancellationToken.token });
 export async function ApiServiceRequest<TViewModel = any>({ baseURL = 'http://localhost:3333', method = 'get', ...rest }: AxiosRequestConfig,
   setLoad: React.Dispatch<React.SetStateAction<boolean>>, setNotification: React.Dispatch<React.SetStateAction<INotification>>) {
+ newCancellationToken();
   let counter = 0;
-  const maxRetry = 2;
-  setLoad(true);
 
-  const api: AxiosInstance = axios.create({ baseURL });
+  setLoad && setLoad(true);
+  const api: AxiosInstance = axios.create({ baseURL: 'http://localhost:3333', cancelToken: cancellationToken.token });
 
   api.interceptors.request.use(function (config) {
+    config.cancelToken = cancellationToken.token;
+    
+     if (!config.url?.includes('login')) {
+       const localStorageToken = localStorage.getItem('@sisag:token') as string;
+       const token = JSON.parse(localStorageToken);
+       config.headers.authorization = `Bearer ${token}`;
+      };
     // Do something before request is sent
     return config;
   }, function (error) {
@@ -25,9 +48,9 @@ export async function ApiServiceRequest<TViewModel = any>({ baseURL = 'http://lo
   }, function (error: AxiosError) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    if (counter < maxRetry) {
+  if (counter < retry) {
       counter += 1;
-      return new Promise((resolve) => resolve(api.request<TViewModel>({ ...rest, method })));
+      return new Promise((resolve) => setTimeout(() => resolve(api.request<TViewModel>({ ...rest, method })), retryDelay));
     };
 
     return Promise.reject(error);
@@ -48,30 +71,34 @@ export async function ApiServiceRequest<TViewModel = any>({ baseURL = 'http://lo
       request: rest,
     } as AxiosResponse<IResponseError>;
 
-
+    //TODO: melhorar lógica de tratamento de erro
     if (error && axios.isAxiosError(error)) {
-      if (error.response?.status !== 500) {
+      if (error.response && error.response.status !== 500 && error.response.status !== 404) {
         axiosResponse = {
           ...axiosResponse,
-          status: error.response?.status as number,
-          data: error.response?.data,
+          status: error.response.status,
+          data: error.response.data,
+          statusText: error.response.statusText,
+          headers: error.response.headers,
+          config: error.response.config,
+          request: error.response.request,
         }
       }
+      
+      //TODO:alterar para um api de contexto
+      if (!axios.isCancel(error) && error.response?.status === 401) {
+        setNotification && setNotification({ tipo: 'error', descricao: (axiosResponse.data as IResponseError).message });
+        setTimeout(() => {
+          localStorage.removeItem('@sisag:token');
+          localStorage.removeItem('@sisag:user');
+          window.location.replace('/');
+        }, 2100);
+      } else {
+        setNotification && setNotification({ tipo: 'error', descricao: (axiosResponse.data as IResponseError).message });
+      }
+    };
+  };
 
-      axiosResponse = {
-        ...axiosResponse,
-        status: error.response?.status as number,
-        statusText: error.response?.statusText as string,
-        headers: error.response?.headers as AxiosRequestConfig,
-        config: error.response?.config as AxiosRequestConfig,
-        request: error.response?.request,
-      };
-
-    }
-
-    setLoad(false);
-    setNotification({ open: true, status: 'error', message: (axiosResponse.data as IResponseError).message });
-  }
-
+  setLoad && setLoad(false);
   return axiosResponse.data;
 };
