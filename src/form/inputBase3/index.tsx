563582934ca8e78e80f1434e1680b9f539e7da05
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-06-11 13:35:21 -0300

    Adicionando tipagem

--- src/form/inputBase3/index.tsx
+++ src/form/inputBase3/index.tsx
@@ -1,67 +1,66 @@
-import { useRef, useEffect } from "react";
+import React, { useRef, useEffect, DetailedHTMLProps, FormHTMLAttributes } from "react";
 
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 function InputField3() {
-  const emailRef = useRef<any>();
-  const formRef = useRef<any>();
+  const emailRef = useRef<HTMLInputElement | undefined>();
+  const formRef = useRef() as React.MutableRefObject<HTMLFormElement>;
   const error = emailRef;
 
   const handleChange = (e: any) => {
     const email = emailRef.current;
-    var test = email.value.length === 0 || emailRegExp.test(email.value);
+    // var test = (email && email.value.length === 0) || emailRegExp.test(email.value);
 
-    if (test) {
-      email.className = "valid";
-      error.current.nextSibling.innerHTML = "";
-      error.current.nextSibling.className = "error";
+    // if (test) {
+    //   email.className = "valid";
+    //   error.current.nextSibling.innerHTML = "";
+    //   error.current.nextSibling.className = "error";
 
-    } else {
-      email.className = "invalid";
-      error.current.nextSibling.className = "error";
-      error.current.nextSibling.innerHTML = "Informe email correto!";
-      error.current.nextSibling.className = "error active";
-    }
+    // } else {
+    //   email.className = "invalid";
+    //   error.current.nextSibling.className = "error";
+    //   error.current.nextSibling.innerHTML = "Informe email correto!";
+    //   error.current.nextSibling.className = "error active";
+    // }
 
     console.log(e.target.value, test);
     console.log();
   };
 
-  function load() {
-    // Here, we test if the field is empty (remember, the field is not required)
-    // If it is not, we check if its content is a well-formed e-mail address.
-    var test = emailRef?.current.value.length === 0 || emailRegExp.test(emailRef.current.value);
-    emailRef.current.className = test ? "valid" : "invalid";
+  const load = () => {
+    // var test = emailRef?.current.value.length === 0 || emailRegExp.test(emailRef.current.value);
+    // emailRef.current.className = test ? "valid" : "invalid";
   };
 
-  function handleSubmit(e: any) {
+  const handleSubmit = (e: any) => {
+    if (!!emailRef.current && !!emailRef.current && !!error.current && !!error.current.nextElementSibling) return;
+
     e.preventDefault();
     const email = emailRef.current;
 
-    var test = email.value.length === 0 || emailRegExp.test(email.value);
+    var test = email?.value.length === 0 || emailRegExp.test(email?.value || '');
 
-    if (!test) {
+    if (!test && email && error.current && error.current.nextElementSibling) {
       email.className = "invalid";
-      error.current.nextSibling.innerHTML = "Informe email correto!";
-      error.current.nextSibling.className = "error active";
-
-      // Some legacy browsers do not support the event.preventDefault() method
+      error.current.nextElementSibling.innerHTML = 'Informe email correto!';
+      error.current.nextElementSibling.className = "error active";
       return false;
     } else {
-      email.className = "valid";
-      error.current.nextSibling.innerHTML = "";
-      error.current.nextSibling.className = "error";
+      // email.className = "valid";
+      // error.current.nextSibling.innerHTML = "";
+      // error.current.nextSibling.className = "error";
     }
   };
 
   useEffect(() => {
-    emailRef.current.addEventListener('load', load);
-    emailRef.current.addEventListener('input', handleChange);
-    formRef.current.addEventListener('submit', handleSubmit);
+
+    // emailRef.current?.addEventListener('load', load);
+    // emailRef.current?.addEventListener('input', handleChange);
+    // formRef.current?.addEventListener('submit', handleSubmit);
 
     return () => {
-      console.log('emailRef.current', emailRef.current);
-      console.log('formRef.current', formRef.current);
+      // console.log('emailRef.current', emailRef.current);
+      // console.log('formRef.current', formRef.current);
       // emailRef.current && emailRef.current?.removeEventListner('load', load);
       // emailRef.current && emailRef.current?.removeEventListner('input', handleChange);
       // formRef.current && formRef.current?.removeEventListner('submit', handleSubmit);
@@ -73,7 +72,7 @@ function InputField3() {
       <p style={{ width: "300px" }}>
         <label htmlFor="mail" style={{ width: "300px" }}>
           <span>Por favor escreva seu email:</span>
-          <input type="text" className="mail" id="mail" name="mail" ref={emailRef} width='100%' />
+          {/* <input type="text" className="mail" id="mail" name="mail" ref={emailRef} width='100%' /> */}
           <span className="error" aria-live="polite"></span>
         </label>
       </p>
