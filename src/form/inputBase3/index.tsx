5a8290d5dab783dcb3c385ad80428380bd979086
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-06-28 12:52:35 -0300

    Adicionando initial validate

--- src/form/inputBase3/index.tsx
+++ src/form/inputBase3/index.tsx
@@ -3,10 +3,15 @@ import './styles.css';
 
 const emailRegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
-function InputField3() {
-  const emailRef = useRef() as React.RefObject<HTMLInputElement>;
+type InputProps = {
+  value?: string;
+};
+
+const InputField3: React.FC<InputProps> = ({
+  value
+}) => {
+  const emailRef = useRef({ defaultValue: value }) as React.RefObject<HTMLInputElement>;
   const formRef = useRef() as React.MutableRefObject<HTMLFormElement>;
-  const error = emailRef;
 
   const handleChange = React.useCallback(function (this: HTMLInputElement, _e: Event) {
     const email = emailRef.current;
@@ -14,19 +19,18 @@ function InputField3() {
 
     if (test) {
       email!.className = 'valid';
-      error!.current!.nextElementSibling!.innerHTML = '';
-      error.current!.nextElementSibling!.className = 'error';
+      emailRef!.current!.nextElementSibling!.innerHTML = '';
+      emailRef.current!.nextElementSibling!.className = 'error';
 
     } else {
       email!.className = 'invalid';
-      error.current!.nextElementSibling!.innerHTML = 'Informe email correto!';
+      emailRef.current!.nextElementSibling!.innerHTML = 'Informe email correto!';
     }
-  }, [error]);
+  }, [emailRef]);
 
   const load = () => {
     const test = emailRef.current!.value.length === 0 || emailRegExp.test(emailRef.current!.value);
     emailRef.current!.className = test ? 'valid' : 'invalid';
-
   };
 
   const handleSubmit = function (event: React.FormEvent<HTMLFormElement>) {
@@ -34,17 +38,32 @@ function InputField3() {
     const email = emailRef.current;
     const test = email?.value.length === 0 || emailRegExp.test(email?.value || '');
 
-    if (!test && email && error.current && error.current.nextElementSibling) {
+    if (!test && email && emailRef.current && emailRef.current.nextElementSibling) {
       email.className = 'invalid';
-      error.current.nextElementSibling.innerHTML = 'Informe email correto!';
-      error.current.nextElementSibling.className = 'error active';
+      emailRef.current.nextElementSibling.innerHTML = 'Informe email correto!';
+      emailRef.current.nextElementSibling.className = 'error active';
       return false;
     } else {
       email!.className = 'valid';
-      error.current!.nextElementSibling!.innerHTML = '';
-      error.current!.nextElementSibling!.className = 'error';
+      emailRef.current!.nextElementSibling!.innerHTML = '';
+      emailRef.current!.nextElementSibling!.className = 'error';
     }
   };
+
+  useEffect(() => {
+    const email = emailRef.current;
+    const test = (email && email.value.length === 0) || emailRegExp.test(email?.value || '');
+
+    if (test) {
+      email!.className = 'valid';
+      emailRef!.current!.nextElementSibling!.innerHTML = '';
+      emailRef.current!.nextElementSibling!.className = 'error';
+
+    } else {
+      email!.className = 'invalid';
+      emailRef.current!.nextElementSibling!.innerHTML = 'Informe email correto!';
+    }
+  }, []);
 
   useEffect(() => {
     emailRef.current?.addEventListener('load', load);
@@ -56,7 +75,7 @@ function InputField3() {
       <p>
         <label htmlFor='mail'>
           <span>Please enter an email address:</span>
-          <input type='text' className='mail' id='mail' name='mail' ref={emailRef} />
+          <input type='text' className='mail' id='mail' name='mail' ref={emailRef} defaultValue={value} />
           <span className='error' aria-live='polite'></span>
         </label>
       </p>
--- src/pages/form-validation/index.tsx
+++ src/pages/form-validation/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 // import InputField2 from '../../form/InputBase2'
+// import InputField4 from '../../form/inputBase4'
 import InputField3 from '../../form/inputBase3'
-import InputField4 from '../../form/inputBase4'
 
 
 const FormValidation = () => {
-  return (
-    <>
-      Adicionar validações para formulários;
-      <InputField3 />
-      <InputField4 />
-    </>
-  )
+  return (<>
+    Adicionar validações para formulários;
+    <InputField3 value={'teste'} />
+    {/* <InputField4 /> */}
+
+  </>)
 }
 
 export default FormValidation
