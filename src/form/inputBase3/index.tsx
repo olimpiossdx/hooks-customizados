e18aaa6bcbe13b463deaef6d40ba96777ffde8e1
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-06-28 12:43:31 -0300

    Atualizando validação

--- src/form/inputBase3/index.tsx
+++ src/form/inputBase3/index.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect } from 'react';
+import './styles.css';
 
-const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailRegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 function InputField3() {
   const emailRef = useRef() as React.RefObject<HTMLInputElement>;
@@ -12,21 +13,19 @@ function InputField3() {
     const test = (email && email.value.length === 0) || emailRegExp.test(email?.value || '');
 
     if (test) {
-      email!.className = "valid";
-      error!.current!.nextElementSibling!.innerHTML = "";
-      error.current!.nextElementSibling!.className = "error";
+      email!.className = 'valid';
+      error!.current!.nextElementSibling!.innerHTML = '';
+      error.current!.nextElementSibling!.className = 'error';
 
     } else {
-      email!.className = "invalid";
-      error.current!.nextElementSibling!.className = "error";
-      error.current!.nextElementSibling!.innerHTML = "Informe email correto!";
-      error.current!.nextElementSibling!.className = "error active";
+      email!.className = 'invalid';
+      error.current!.nextElementSibling!.innerHTML = 'Informe email correto!';
     }
   }, [error]);
 
   const load = () => {
     const test = emailRef.current!.value.length === 0 || emailRegExp.test(emailRef.current!.value);
-    emailRef.current!.className = test ? "valid" : "invalid";
+    emailRef.current!.className = test ? 'valid' : 'invalid';
 
   };
 
@@ -36,14 +35,14 @@ function InputField3() {
     const test = email?.value.length === 0 || emailRegExp.test(email?.value || '');
 
     if (!test && email && error.current && error.current.nextElementSibling) {
-      email.className = "invalid";
+      email.className = 'invalid';
       error.current.nextElementSibling.innerHTML = 'Informe email correto!';
-      error.current.nextElementSibling.className = "error active";
+      error.current.nextElementSibling.className = 'error active';
       return false;
     } else {
-      email!.className = "valid";
-      error.current!.nextElementSibling!.innerHTML = "";
-      error.current!.nextElementSibling!.className = "error";
+      email!.className = 'valid';
+      error.current!.nextElementSibling!.innerHTML = '';
+      error.current!.nextElementSibling!.className = 'error';
     }
   };
 
@@ -52,16 +51,18 @@ function InputField3() {
     emailRef.current?.addEventListener('input', handleChange);
   }, [handleChange]);
 
-  return (<form ref={formRef} onSubmit={handleSubmit} style={{ width: '100%' }}>
-    <p style={{ width: "300px" }}>
-      <label htmlFor="mail" style={{ width: "300px" }}>
-        <span>Por favor escreva seu email:</span>
-        <input type="text" className="mail" id="mail" name="mail" ref={emailRef} width='100%' />
-        <span className="error" aria-live="polite"></span>
-      </label>
-    </p>
-    <button type="submit">Submit</button>
-  </form>);
-}
+  return (<>
+    <form ref={formRef} onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor='mail'>
+          <span>Please enter an email address:</span>
+          <input type='text' className='mail' id='mail' name='mail' ref={emailRef} />
+          <span className='error' aria-live='polite'></span>
+        </label>
+      </p>
+      <button type='submit'>Submit</button>
+    </form>
+  </>);
+};
 
 export default InputField3;
