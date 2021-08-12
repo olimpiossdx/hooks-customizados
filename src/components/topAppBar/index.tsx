2fd0fbb74bdde616a3d1381595dec84129bc20d1
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-08-12 13:10:14 -0300

    Tradução de class para componente

--- src/components/topAppBar/index.tsx
+++ src/components/topAppBar/index.tsx
@@ -1,5 +1,5 @@
 import { EmotionJSX } from '@emotion/react/types/jsx-namespace'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 interface ITopAppBar {
   navIcon: EmotionJSX.Element;
@@ -9,9 +9,25 @@ interface ITopAppBar {
   alwaysCollapsed: boolean;
   title: string;
   actionItems(): EmotionJSX.Element[] | null;
+  className?: any;
 };
 
-const TopAppBar: React.FC<ITopAppBar> = () => {
+const TopAppBar: React.FC<ITopAppBar> = ({ alwaysCollapsed = false, short = false, fixed = false, prominent = false, title = '', actionItems = null, navIcon = null, className }) => {
+  const [foundation_, setFoundation_] = useState(null);
+  const topAppBarElement = useRef();
+  const [state, setstate] = useState({ classList: new Set(), style: {} });
+
+  function classes() {
+    const { classList } = state;
+
+    return className('mdc-top-app-bar', Array.from(classList), className, {
+      'mdc-top-app-bar--fixed': fixed,
+      'mdc-top-app-bar--short': short,
+      'mdc-top-app-bar--short-collapsed': alwaysCollapsed,
+      'mdc-top-app-bar--prominent': prominent,
+    });
+  };
+
   return (
     <div>
 
